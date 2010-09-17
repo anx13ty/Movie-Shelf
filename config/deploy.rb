d1afc2ac4a084ae81078e5923b9f1f694afcb666
Brent Bahry <brentbahry@gmail.com>
2010-09-17 10:07:38 -0700

    more updates to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,15 +5,20 @@ set :application, "movieshelf.brentbahry.com"
 set :applicationdir, "/home/#{user}/#{application}"
 
 # set :application, "movieshelf"
-set :repository,  "set your repository location here"
+set :repository,  "#{user}@#{domain}/~/git/movieshelf.git"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1  # for rails vendored plugins
+set :branch, "master"
+set :git_shallow_clone, 1
+set :scm_verbose, true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
